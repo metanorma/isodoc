b0a998fcf9d289d1af1c9698ab364b1ecf06300f
Nick Nicholas <opoudjis@optusnet.com.au>
2018-03-03 23:50:38 +1100

    Set up rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+require "bundler/setup"
+require "asciidoctor"
+require "isodoc"
+require "rspec/matchers"
+require "equivalent-xml"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+def strip_guid(x)
+  x.gsub(%r{ id="_[^"]+"}, ' id="_"').gsub(%r{ target="_[^"]+"}, ' target="_"')
+end
+
