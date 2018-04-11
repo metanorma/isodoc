2adac1ecd49461a5cf2c5a9daa917115ee1783cc
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-11 22:43:18 +1000

    refine ns()

--- lib/isodoc/utils.rb
+++ lib/isodoc/utils.rb
@@ -3,8 +3,8 @@ module IsoDoc
     def ns(xpath)
       xpath.gsub(%r{/([a-zA-z])}, "/xmlns:\\1").
         gsub(%r{::([a-zA-z])}, "::xmlns:\\1").
-        gsub(%r{\[([a-zA-z]+ ?=)}, "[xmlns:\\1").
-        gsub(%r{\[([a-zA-z]+\])}, "[xmlns:\\1")
+        gsub(%r{\[([a-zA-z][a-z0-9A-Z@/]* ?=)}, "[xmlns:\\1").
+        gsub(%r{\[([a-zA-z][a-z0-9A-Z@/]*\])}, "[xmlns:\\1")
     end
 
     def insert_tab(out, n)
