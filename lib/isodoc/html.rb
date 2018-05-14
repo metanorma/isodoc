46fa96d5b6aadc3c2647dfc212163eecad97519b
Nick Nicholas <opoudjis@optusnet.com.au>
2018-05-14 23:58:18 +1000

    html footnote:

--- lib/isodoc/html.rb
+++ lib/isodoc/html.rb
@@ -151,8 +151,8 @@ module IsoDoc
         seen[x["href"]] = true
         fn = docxml.at(%<//*[@id = '#{x['href'].sub(/^#/, '')}']>) || next
         x["id"] || x["id"] = "fnref:#{i + 1}"
-        fn.elements.first.children.first.previous =
-          "<a href='##{x['id']}'>#{x.at('./sup').text}) </a>"
+        fn.elements.first.children.first.previous = x.at('./sup').text
+        fn.add_child << "<a href='##{x['id']}'>&#x21A9;</a>"
       end
       docxml
     end
