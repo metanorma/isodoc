929ca39851e4c58274f226c0046287fd13fe1f5c
Nick Nicholas <opoudjis@optusnet.com.au>
2018-05-15 00:23:40 +1000

    footnote backlink:

--- lib/isodoc/html.rb
+++ lib/isodoc/html.rb
@@ -152,7 +152,7 @@ module IsoDoc
         fn = docxml.at(%<//*[@id = '#{x['href'].sub(/^#/, '')}']>) || next
         x["id"] || x["id"] = "fnref:#{i + 1}"
         fn.elements.first.children.first.previous = x.dup
-        fn.last_element_child.add_child "&nbsp;<a href='##{x['id']}'>&#x21A9;</a>"
+        fn.add_child "<a href='##{x['id']}'>&#x21A9;</a>"
       end
       docxml
     end
