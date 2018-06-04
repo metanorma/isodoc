ecc5a5b8537b543d0635017872db55155d514889
Nick Nicholas <opoudjis@optusnet.com.au>
2018-06-04 23:56:10 +1000

    htmlconvert

---
+++ lib/isodoc/htmlconvert/convert.rb
@@ -0,0 +1,13 @@
+require "uuidtools"
+require "html2doc"
+require "liquid"
+
+require_relative "comments.rb"
+require_relative "footnotes.rb"
+require_relative "html.rb"
+
+module IsoDoc
+
+    class Convert < Common
+    end 
+end
