770d849aff1c5175425dda976e5c62706f9e9193
Nick Nicholas <opoudjis@optusnet.com.au>
2018-08-06 09:50:57 +1000

    anchors for level 1 preface clauses

--- lib/isodoc/function/xref_sect_gen.rb
+++ lib/isodoc/function/xref_sect_gen.rb
@@ -28,6 +28,8 @@ module IsoDoc::Function
     # in StanDoc, prefaces have no numbering; they are referenced only by title
     def preface_names(clause)
       return if clause.nil?
+      @anchors[clause["id"]] =
+        { label: nil, level: 1, xref: c.at(ns("./title"))&.text }
       clause.xpath(ns("./clause")).each_with_index do |c, i|
         preface_names1(c, c.at(ns("./title"))&.text, "#{clause.at(ns('./title'))&.text}, #{i+1}", 2)
       end
