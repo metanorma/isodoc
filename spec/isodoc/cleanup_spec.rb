71411b88edacb140125090efb813c7222f07a4e1
Nick Nicholas <opoudjis@gmail.com>
2019-10-26 19:50:59 +1100

    footnote testing

--- spec/isodoc/cleanup_spec.rb
+++ spec/isodoc/cleanup_spec.rb
@@ -150,71 +150,115 @@ RSpec.describe IsoDoc do
 
   it "cleans up footnotes" do
     expect(IsoDoc::HtmlConvert.new({}).cleanup(Nokogiri::XML(<<~"INPUT")).to_s).to be_equivalent_to <<~"OUTPUT"
-              <html xmlns:epub="http://www.idpf.org/2007/ops">
-         <head>
-           <title>test</title>
-           <body lang="EN-US" link="blue" vlink="#954F72">
-             <div class="WordSection1">
-               <p>&#160;</p>
-             </div>
-             <br clear="all" class="section"/>
-             <div class="WordSection2">
-               <br clear="all" style="mso-special-character:line-break;page-break-before:always"/>
+    #{HTML_HDR}
+    <br/>
                <div>
                  <h1 class="ForewordTitle">Foreword</h1>
-                 <p>A.<a href="#ftn2" epub:type="footnote"><sup>2</sup></a></p>
-                 <p>B.<a href="#ftn2" epub:type="footnote"><sup>2</sup></a></p>
-                 <p>C.<a href="#ftn1" epub:type="footnote"><sup>1</sup></a></p>
+                <p>A.<a rel="footnote" href="#fn:2" epub:type="footnote"><sup>2</sup></a></p>
+                <p>B.<a rel="footnote" href="#fn:2" epub:type="footnote"><sup>2</sup></a></p>
+                <p>C.<a rel="footnote" href="#fn:1" epub:type="footnote"><sup>1</sup></a></p>
                </div>
-               <p>&#160;</p>
-             </div>
-             <br clear="all" class="section"/>
-             <div class="WordSection3">
                <p class="zzSTDTitle1"/>
-               <aside id="ftn2">
-         <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
-       </aside>
-               <aside id="ftn1">
-         <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Hello! denoted as 15 % (m/m).</p>
-       </aside>
+                             <aside id="fn:2" class="footnote">
+          <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
+        </aside>
+              <aside id="fn:1" class="footnote">
+          <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Hello! denoted as 15 % (m/m).</p>
+        </aside>
              </div>
            </body>
-         </head>
        </html>
        INPUT
-             <?xml version="1.0"?>
-       <html xmlns:epub="http://www.idpf.org/2007/ops">
-         <head>
-           <title>test</title>
+       #{HTML_HDR}
+       <br/>
+                  <div>
+                    <h1 class="ForewordTitle">Foreword</h1>
+                   <p>A.<a rel="footnote" href="#fn:2" epub:type="footnote"><sup>1</sup></a></p>
+                   <p>B.<a rel="footnote" href="#fn:2" epub:type="footnote"><sup>2</sup></a></p>
+                   <p>C.<a rel="footnote" href="#fn:1" epub:type="footnote"><sup>3</sup></a></p>
+                  </div>
+                  <p class="zzSTDTitle1"/>
+                                <aside id="fn:2" class="footnote">
+             <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
+           </aside>
+                 <aside id="fn:1" class="footnote">
+             <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Hello! denoted as 15 % (m/m).</p>
+           </aside>
+                </div>
+              </body>
+          </html>
+    OUTPUT
+  end
+
+    it "cleans up footnotes (Word)" do
+    expect(IsoDoc::WordConvert.new({}).cleanup(Nokogiri::XML(<<~"INPUT")).to_s).to be_equivalent_to <<~"OUTPUT"
+     <html xmlns:epub="http://www.idpf.org/2007/ops" lang="en">
+         <head/>
            <body lang="EN-US" link="blue" vlink="#954F72">
              <div class="WordSection1">
-               <p>&#xA0;</p>
+               <p>&#160;</p>
              </div>
-             <br clear="all" class="section"/>
+             <p><br clear="all" class="section"/></p>
              <div class="WordSection2">
-               <br clear="all" style="mso-special-character:line-break;page-break-before:always"/>
+               <p><br clear="all" style="mso-special-character:line-break;page-break-before:always"/></p>
                <div>
                  <h1 class="ForewordTitle">Foreword</h1>
-                 <p>A.<a href="#ftn2" epub:type="footnote"><sup>1</sup></a></p>
+                 <p>A.<a href="#ftn1" epub:type="footnote"><sup>1</sup></a></p>
                  <p>B.<a href="#ftn2" epub:type="footnote"><sup>2</sup></a></p>
-                 <p>C.<a href="#ftn1" epub:type="footnote"><sup>3</sup></a></p>
+                 <p>C.<a href="#ftn3" epub:type="footnote"><sup>3</sup></a></p>
                </div>
-               <p>&#xA0;</p>
+               <p>&#160;</p>
              </div>
-             <br clear="all" class="section"/>
+             <p><br clear="all" class="section"/></p>
              <div class="WordSection3">
                <p class="zzSTDTitle1"/>
+               <aside id="ftn1">
+         <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
+       </aside>
                <aside id="ftn2">
          <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
        </aside>
-               <aside id="ftn1">
+               <aside id="ftn3">
          <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Hello! denoted as 15 % (m/m).</p>
        </aside>
              </div>
            </body>
-         </head>
        </html>
-    OUTPUT
+   INPUT
+   <?xml version="1.0"?>
+       <html xmlns:epub="http://www.idpf.org/2007/ops" lang="en">
+           <head/>
+             <body lang="EN-US" link="blue" vlink="#954F72">
+               <div class="WordSection1">
+                 <p>&#xA0;</p>
+               </div>
+               <p><br clear="all" class="section"/></p>
+               <div class="WordSection2">
+                 <p><br clear="all" style="mso-special-character:line-break;page-break-before:always"/></p>
+                 <div>
+                   <h1 class="ForewordTitle">Foreword</h1>
+                   <p>A.<a href="#ftn1" epub:type="footnote"><sup>1</sup></a></p>
+                   <p>B.<a href="#ftn2" epub:type="footnote"><sup>2</sup></a></p>
+                   <p>C.<a href="#ftn3" epub:type="footnote"><sup>3</sup></a></p>
+                 </div>
+                 <p>&#xA0;</p>
+               </div>
+               <p><br clear="all" class="section"/></p>
+               <div class="WordSection3">
+                 <p class="zzSTDTitle1"/>
+                 <aside id="ftn1">
+           <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
+         </aside>
+                 <aside id="ftn2">
+           <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Formerly denoted as 15 % (m/m).</p>
+         </aside>
+                 <aside id="ftn3">
+           <p id="_1e228e29-baef-4f38-b048-b05a051747e4">Hello! denoted as 15 % (m/m).</p>
+         </aside>
+               </div>
+             </body>
+         </html>
+   OUTPUT
   end
 
   it "cleans up tables with tfoot" do
