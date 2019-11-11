a3bf68d3d80cb25a6230608c83e0b26090514147
Nick Nicholas <opoudjis@gmail.com>
2019-11-12 10:48:20 +1100

    deal with asterisk image type

--- lib/isodoc/function/utils.rb
+++ lib/isodoc/function/utils.rb
@@ -158,6 +158,7 @@ module IsoDoc::Function
       #@files_to_delete << new_file
       #File.open(new_file, "wb") { |f| f.write(Base64.strict_decode64(imgdata)) }
       #File.join(relative_dir ? rel_tmpimagedir : tmpimagedir, fname)
+      imgtype = "png" unless /^[a-z0-9]+$/.match imgtype
       Tempfile.open(["image", ".#{imgtype}"]) do |f|
         f.binmode
         f.write(Base64.strict_decode64(imgdata))
