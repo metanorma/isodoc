metanorma/isodoc

commit 440cd039a4fffc8c7dfecf351cf138e819c623b4

Fix rubocop issues in lib/isodoc/xslfo_convert.rb

lib/isodoc/xslfo_convert.rb
@@ -2,7 +2,6 @@ require "metanorma"
 
 module IsoDoc
   class XslfoPdfConvert < ::IsoDoc::Convert
-
     def initialize(options)
       super
       @maxwidth = 500
@@ -19,11 +18,11 @@ module IsoDoc
       super
     end
 
-    def pdf_stylesheet(docxml)
+    def pdf_stylesheet(_docxml)
       nil
     end
 
-    def pdf_options(docxml)
+    def pdf_options(_docxml)
       if font_manifest_file = @options.dig(:mn2pdf, :font_manifest_file)
         "--font-manifest #{font_manifest_file}"
       else
@@ -31,25 +30,39 @@ module IsoDoc
       end
     end
 
-    def convert(input_filename, file = nil, debug = false, output_filename = nil)
+    def convert(input_filename, file = nil, debug = false,
+                output_filename = nil)
       file = File.read(input_filename, encoding: "utf-8") if file.nil?
-      docxml, filename, dir = convert_init(file, input_filename, debug)
-      /\.xml$/.match(input_filename) or
-          input_filename = Tempfile.open([filename, ".xml"], encoding: "utf-8") do |f|
-          f.write file
-          f.path
-        end
-      FileUtils.rm_rf dir
-      ::Metanorma::Output::XslfoPdf.new.convert(input_filename,
-                                                output_filename || "#{filename}.#{@suffix}",
-                                               File.join(@libdir, pdf_stylesheet(docxml)),
-                                               pdf_options(docxml))
+      input_filename, docxml = input_xml_path(input_filename, file, debug)
+      ::Metanorma::Output::XslfoPdf.new.convert(
+        input_filename,
+        output_filename || "#{filename}.#{@suffix}",
+        File.join(@libdir, pdf_stylesheet(docxml)),
+        pdf_options(docxml)
+      )
     end
 
     def xref_parse(node, out)
-      target = /#/.match(node["target"]) ? node["target"].sub(/#/, ".pdf#") :
-        "##{node["target"]}"
+      target = if /#/.match?(node["target"])
+                 node["target"].sub(/#/, ".pdf#")
+               else
+                 "##{node['target']}"
+               end
       out.a(**{ "href": target }) { |l| l << get_linkend(node) }
+    end
+
+    def input_xml_path(input_filename, xml_file, debug)
+      docxml, filename, dir = convert_init(xml_file, input_filename, debug)
+      unless /\.xml$/.match?(input_filename)
+        input_filename = Tempfile.open([filename, ".xml"],
+                                       encoding: "utf-8") do |f|
+          f.write xml_file
+          f.path
+        end
+      end
+      FileUtils.rm_rf dir
+
+      [input_filename, docxml]
     end
   end
 end

commit 0f9105fb398aa41063291370f152575dd4af869e

Fix missing filename variable, regression after 440cd039

lib/isodoc/xslfo_convert.rb
@@ -33,7 +33,8 @@ module IsoDoc
     def convert(input_filename, file = nil, debug = false,
                 output_filename = nil)
       file = File.read(input_filename, encoding: "utf-8") if file.nil?
-      input_filename, docxml = input_xml_path(input_filename, file, debug)
+      input_filename, docxml, filename = input_xml_path(input_filename,
+                                                        file, debug)
       ::Metanorma::Output::XslfoPdf.new.convert(
         input_filename,
         output_filename || "#{filename}.#{@suffix}",
@@ -57,7 +58,7 @@ module IsoDoc
       end
       FileUtils.rm_rf dir
 
-      [input_filename, docxml]
+      [input_filename, docxml, filename]
     end
   end
 end