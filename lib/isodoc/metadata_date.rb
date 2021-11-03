67bf27ecee573eaacb39175be070a9047a51dcdf
Nick Nicholas <opoudjis@gmail.com>
2021-11-04 02:03:45 +1100

    uniform processing of SVG as data uri: was missing +xml mime type: https://github.com/metanorma/metanorma-ogc/issues/305

--- lib/isodoc/metadata_date.rb
+++ lib/isodoc/metadata_date.rb
@@ -6,7 +6,7 @@ module IsoDoc
                    vote-ended}.freeze
 
     def months
-    {
+      {
         "01": @labels["month_january"],
         "02": @labels["month_february"],
         "03": @labels["month_march"],
@@ -19,30 +19,32 @@ module IsoDoc
         "10": @labels["month_october"],
         "11": @labels["month_november"],
         "12": @labels["month_december"],
-    }
+      }
     end
 
     def monthyr(isodate)
       m = /(?<yr>\d\d\d\d)-(?<mo>\d\d)/.match isodate
       return isodate unless m && m[:yr] && m[:mo]
+
       l10n("#{months[m[:mo].to_sym]} #{m[:yr]}",
-                                   @lang, @script)
+           @lang, @script)
     end
 
     def MMMddyyyy(isodate)
       return nil if isodate.nil?
+
       arr = isodate.split("-")
-      date = if arr.size == 1 and (/^\d+$/.match isodate)
-               Date.new(*arr.map(&:to_i)).strftime("%Y")
-             elsif arr.size == 2
-               Date.new(*arr.map(&:to_i)).strftime("%B %Y")
-             else
-               Date.parse(isodate).strftime("%B %d, %Y")
-             end
+      if arr.size == 1 && (/^\d+$/.match isodate)
+        Date.new(*arr.map(&:to_i)).strftime("%Y")
+      elsif arr.size == 2
+        Date.new(*arr.map(&:to_i)).strftime("%B %Y")
+      else
+        Date.parse(isodate).strftime("%B %d, %Y")
+      end
     end
 
     def bibdate(isoxml, _out)
-      isoxml.xpath(ns('//bibdata/date')).each do |d|
+      isoxml.xpath(ns("//bibdata/date")).each do |d|
         set("#{d['type'].gsub(/-/, '_')}date".to_sym, Common::date_range(d))
       end
     end
