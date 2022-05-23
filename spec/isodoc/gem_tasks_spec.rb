849cf0cb80c876267ad5ba6eb3445b0c23bdaedc
Alexande B <abobrikovich@gmail.com>
2022-05-23 21:39:31 +0200

    #403 fix css generation logic, not rely on CLEAN global FileList

---
+++ spec/isodoc/gem_tasks_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+require "rake"
+
+RSpec.describe "IsoDoc::GemTasks" do
+  before :all do
+    @rake_app_orig = Rake.application
+    Rake.application = Rake::Application.new
+    Rake.application.load_rakefile
+  end
+
+  after :all do
+    Rake.application = @rake_app_orig
+  end
+
+  around do |example|
+    ci_env_orig = ENV.fetch("CI", nil)
+    ENV["CI"] = "true"
+    example.run
+    ENV["CI"] = ci_env_orig
+  end
+
+  it "test CLEAN contains CSS" do
+    expect(CLEAN).to include *IsoDoc::GemTasks.css_list
+  end
+
+  it "test build deps has build_scss" do
+    build_deps = Rake::Task["build"].all_prerequisite_tasks.map(&:name)
+    expect(build_deps).to include "build_scss"
+  end
+
+  it "test generate/clean CSS" do
+    Rake.application.invoke_task "build"
+
+    expect(IsoDoc::GemTasks.css_list).not_to be_empty
+  end
+end
