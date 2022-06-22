50ab6117ceb3dcd04890253662accc317dbe8cf8
Nick Nicholas <opoudjis@gmail.com>
2022-06-23 00:47:49 +1000

    process suppress_identifier bibitems: https://github.com/metanorma/metanorma-bipm/issues/176

--- spec/isodoc/ref_spec.rb
+++ spec/isodoc/ref_spec.rb
@@ -333,7 +333,7 @@ RSpec.describe IsoDoc do
                   &#160; This is an annotation of ISO 20483:2013-2014
                 </p>
               </div>
-              <p id='zip_ffs' class='NormRef'>[5], Title 5</p>
+              <p id='zip_ffs' class='NormRef'>[5] Title 5</p>
             </div>
             <br/>
             <div>
@@ -982,51 +982,88 @@ RSpec.describe IsoDoc do
       </iso-standard>
     INPUT
     presxml = <<~PRESXML
-           <bibliography>
-         <references id='_normative_references' obligation='informative' normative='true' displayorder='1'>
-           <title depth='1'>
-             1.
-             <tab/>
-             Normative References
-           </title>
-           <p>
-             The following documents are referred to in the text in such a way that
-             some or all of their content constitutes requirements of this document.
-             For dated references, only the edition cited applies. For undated
-             references, the latest edition of the referenced document (including any
-             amendments) applies.
-           </p>
-           <bibitem id='ISO712' type='standard' suppress_identifier='true'>
-             <formattedref>
-               International Organization for Standardization.
-               <em>Cereals and cereal products</em>
-               .
-             </formattedref>
-             <docidentifier type='ISO'>ISO 712</docidentifier>
-             <docidentifier type='metanorma'>[110]</docidentifier>
-           </bibitem>
-           <bibitem id='ref1' suppress_identifier='true'>
-             <formattedref format='application/x-isodoc+xml'>
-               <smallcap>Standard No I.C.C 167</smallcap>
-               .
-               <em>
-                 Determination of the protein content in cereal and cereal products for
-                 food and animal feeding stuffs according to the Dumas combustion
-                 method
-               </em>
-                (see
-               <link target='http://www.icc.or.at'/>
-               )
-             </formattedref>
-             <docidentifier type='ICC'>ICC/167</docidentifier>
-           </bibitem>
-         </references>
-       </bibliography>
+          <bibliography>
+        <references id='_normative_references' obligation='informative' normative='true' displayorder='1'>
+          <title depth='1'>
+            1.
+            <tab/>
+            Normative References
+          </title>
+          <p>
+            The following documents are referred to in the text in such a way that
+            some or all of their content constitutes requirements of this document.
+            For dated references, only the edition cited applies. For undated
+            references, the latest edition of the referenced document (including any
+            amendments) applies.
+          </p>
+          <bibitem id='ISO712' type='standard' suppress_identifier='true'>
+            <formattedref>
+              International Organization for Standardization.
+              <em>Cereals and cereal products</em>
+              .
+            </formattedref>
+            <docidentifier type='ISO'>ISO 712</docidentifier>
+            <docidentifier type='metanorma'>[110]</docidentifier>
+          </bibitem>
+          <bibitem id='ref1' suppress_identifier='true'>
+            <formattedref format='application/x-isodoc+xml'>
+              <smallcap>Standard No I.C.C 167</smallcap>
+              .
+              <em>
+                Determination of the protein content in cereal and cereal products for
+                food and animal feeding stuffs according to the Dumas combustion
+                method
+              </em>
+               (see
+              <link target='http://www.icc.or.at'/>
+              )
+            </formattedref>
+            <docidentifier type='ICC'>ICC/167</docidentifier>
+          </bibitem>
+        </references>
+      </bibliography>
     PRESXML
-    expect(xmlpp(Nokogiri::XML(
-      IsoDoc::PresentationXMLConvert.new({})
-      .convert("test", input, true),
-    ).at("//xmlns:bibliography").to_xml))
+    html = <<~OUTPUT
+      #{HTML_HDR}
+             <p class='zzSTDTitle1'/>
+             <div>
+               <h1>1.&#xa0; Normative References</h1>
+               <p>
+                 The following documents are referred to in the text in such a way that
+                 some or all of their content constitutes requirements of this
+                 document. For dated references, only the edition cited applies. For
+                 undated references, the latest edition of the referenced document
+                 (including any amendments) applies.
+               </p>
+               <p id='ISO712' class='NormRef'>
+                 [110] International Organization for Standardization.
+                 <i>Cereals and cereal products</i>
+                 .
+               </p>
+               <p id='ref1' class='NormRef'>
+                 <span style='font-variant:small-caps;'>Standard No I.C.C 167</span>
+                 .
+                 <i>
+                   Determination of the protein content in cereal and cereal products
+                   for food and animal feeding stuffs according to the Dumas combustion
+                   method
+                 </i>
+                  (see
+                 <a href='http://www.icc.or.at'>http://www.icc.or.at</a>
+                 )
+               </p>
+             </div>
+           </div>
+         </body>
+       </html>
+    OUTPUT
+    doc = IsoDoc::PresentationXMLConvert.new({})
+      .convert("test", input, true)
+    expect(xmlpp(Nokogiri::XML(doc)
+    .at("//xmlns:bibliography").to_xml))
       .to be_equivalent_to xmlpp(presxml)
+    expect(xmlpp(IsoDoc::HtmlConvert.new({})
+   .convert("test", doc, true)))
+      .to be_equivalent_to xmlpp(html)
   end
 end
