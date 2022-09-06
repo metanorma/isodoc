0bc3cdf9bef86274fb3e01a3937863c8aa3a1546
Nick Nicholas <opoudjis@gmail.com>
2022-09-06 20:44:35 +1000

    rspec: https://github.com/metanorma/metanorma-iso/issues/766

--- spec/requirements/modspec/requirement_components_spec.rb
+++ spec/requirements/modspec/requirement_components_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe Metanorma::Requirements::Modspec do
       <inherit>/ss/584/2015/level/1</inherit>
       <subject>user</subject>
       <description><p id="_">I recommend <em>1</em>.</p></description>
-      <component class="test-purpose"><p>TEST PURPOSE</p></component>
+      <component class="test-purpose" id="A1"><p>TEST PURPOSE</p></component>
       <description><p id="_">I recommend <em>2</em>.</p></description>
-      <component class="conditions"><p>CONDITIONS</p></component>
+      <component class="conditions" id="A2"><p>CONDITIONS</p></component>
       <description><p id="_">I recommend <em>3</em>.</p></description>
-      <component class="part"><p>FIRST PART</p></component>
+      <component class="part" id="A3"><p>FIRST PART</p></component>
       <description><p id="_">I recommend <em>4</em>.</p></description>
-      <component class="part"><p>SECOND PART</p></component>
+      <component class="part" id="A4"><p>SECOND PART</p></component>
       <description><p id="_">I recommend <em>5</em>.</p></description>
-      <component class="test-method"><p>TEST METHOD</p></component>
+      <component class="test-method" id="A5"><p>TEST METHOD</p></component>
       <description><p id="_">I recommend <em>6</em>.</p></description>
-      <component class="part"><p>THIRD PART</p></component>
+      <component class="part" id="A6"><p>THIRD PART</p></component>
       <description><p id="_">I recommend <em>7</em>.</p></description>
-      <component class="panda GHz express"><p>PANDA PART</p></component>
+      <component class="panda GHz express" id="A7"><p>PANDA PART</p></component>
       <description><p id="_">I recommend <em>8</em>.</p></description>
       </recommendation>
       </foreword>
@@ -30,7 +30,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
       </ogc-standard>
     INPUT
     presxml = <<~OUTPUT
-      <ogc-standard xmlns='https://standards.opengeospatial.org/document' type='presentation'>
+           <ogc-standard xmlns='https://standards.opengeospatial.org/document' type='presentation'>
          <preface>
            <foreword id='A' displayorder='1'>
              <title>Preface</title>
@@ -44,9 +44,10 @@ RSpec.describe Metanorma::Requirements::Modspec do
                </thead>
                <tbody>
                  <tr>
-                  <td scope='colgroup' colspan='2'>
-                  <tt>/ogc/recommendation/wfs/2</tt></td>
-                </tr>
+                   <td scope='colgroup' colspan='2'>
+                     <tt>/ogc/recommendation/wfs/2</tt>
+                   </td>
+                 </tr>
                  <tr>
                    <td>Subject</td>
                    <td>user</td>
@@ -64,7 +65,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A1'>
                    <td>Test purpose</td>
                    <td>
                      <p>TEST PURPOSE</p>
@@ -79,7 +80,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A2'>
                    <td>Conditions</td>
                    <td>
                      <p>CONDITIONS</p>
@@ -94,7 +95,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A3'>
                    <td>A</td>
                    <td>
                      <p>FIRST PART</p>
@@ -109,7 +110,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A4'>
                    <td>B</td>
                    <td>
                      <p>SECOND PART</p>
@@ -124,7 +125,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A5'>
                    <td>Test method</td>
                    <td>
                      <p>TEST METHOD</p>
@@ -139,7 +140,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A6'>
                    <td>C</td>
                    <td>
                      <p>THIRD PART</p>
@@ -154,7 +155,7 @@ RSpec.describe Metanorma::Requirements::Modspec do
                      </p>
                    </td>
                  </tr>
-                 <tr>
+                 <tr id='A7'>
                    <td>Panda GHz express</td>
                    <td>
                      <p>PANDA PART</p>
