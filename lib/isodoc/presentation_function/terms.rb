b41d4ee09fe1d29a764ae0defc9c2670cd41837b
Nick Nicholas <opoudjis@gmail.com>
2022-11-22 21:43:51 +1100

    https://github.com/metanorma/metanorma-iso/issues/852

--- lib/isodoc/presentation_function/terms.rb
+++ lib/isodoc/presentation_function/terms.rb
@@ -24,7 +24,7 @@ module IsoDoc
       concept1_linkmention(ref, render, opts)
       concept1_ref(node, ref, opts)
       concept1_nonital(node, opts)
-      node.replace(node.children.to_xml.strip)
+      node.replace(node.children)
     end
 
     def concept1_nonital(node, opts)
