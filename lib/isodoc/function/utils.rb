90d3c06bb3e48da5aa4c7e5a4ac3aab18030cb63
Maxim Samsonov <m.samsonov@computer.org>
2023-08-28 18:10:52 +0300

    Fixing tmp file handling (4)

--- lib/isodoc/function/utils.rb
+++ lib/isodoc/function/utils.rb
@@ -177,7 +177,8 @@ module IsoDoc
         imgtype = imgtype.sub(/\+[a-z0-9]+$/, "") # svg+xml
         imgtype = "png" unless /^[a-z0-9]+$/.match? imgtype
         imgtype == "postscript" and imgtype = "eps"
-        Tempfile.open(["image", ".#{imgtype}"], mode: File::BINARY) do |f|
+        Tempfile.open(["image", ".#{imgtype}"]) do |f|
+          f.binmode
           f.write(Base64.strict_decode64(imgdata))
           @tempfile_cache << f # persist to the end
           f.path
