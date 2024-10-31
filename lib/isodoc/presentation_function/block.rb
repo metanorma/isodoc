e19096216adfa0adbf7b347323c81d7c3a206b78
Nick Nicholas <opoudjis@gmail.com>
2024-11-01 00:10:39 +1100

    move note_delim to Presentation XML: https://github.com/metanorma/isodoc/issues/606

--- lib/isodoc/presentation_function/block.rb
+++ lib/isodoc/presentation_function/block.rb
@@ -58,6 +58,10 @@ module IsoDoc
       docxml.xpath(ns("//note")).each { |f| note1(f) }
     end
 
+    def note_delim
+      ""
+    end
+
     def note1(elem)
       %w(bibdata bibitem).include?(elem.parent.name) ||
         elem["notag"] == "true" and return
@@ -65,7 +69,7 @@ module IsoDoc
       lbl = @i18n.note
       (n.nil? || n[:label].nil? || n[:label].empty?) or
         lbl = l10n("#{lbl} #{n[:label]}")
-      prefix_name(elem, "", lbl, "name")
+      prefix_name(elem, note_delim, lbl, "name")
     end
 
     def admonition(docxml)
