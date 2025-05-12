0bc41db4ddd348d537a7b30cb711d18a39bfce1a
Nick Nicholas <opoudjis@gmail.com>
2025-05-12 11:14:38 +1000

    version bump

--- lib/isodoc/word_function/footnotes.rb
+++ lib/isodoc/word_function/footnotes.rb
@@ -10,15 +10,6 @@ module IsoDoc
         sprintf "%09d", ret
       end
 
-      # KILL
-      def make_table_footnote_linkx(out, fnid, node)
-        attrs = { href: "##{fnid}", class: "TableFootnoteRef" }
-        sup = node.at(ns(".//sup")) and sup.replace(sup.children)
-        out.a **attrs do |a|
-          children_parse(node, a)
-        end
-      end
-
       def fmt_fn_body_parse(node, out)
         node.at(ns(".//fmt-fn-label"))&.remove
         aside = node.parent.name == "fmt-footnote-container"
@@ -27,27 +18,6 @@ module IsoDoc
         out.send tag, id: "ftn#{id}" do |div|
           node.children.each { |n| parse(n, div) }
         end
-      end
-
-      # KILL
-      def get_table_ancestor_idx(node)
-        table = node.ancestors("table")
-        table.empty? and table = node.ancestors("figure")
-        table.empty? and return [nil,
-                                 UUIDTools::UUID.random_create.to_s]
-        [table.last, table.last["id"]]
-      end
-
-      # KILL
-      def table_footnote_parsex(node, out)
-        fn = node["reference"] || UUIDTools::UUID.random_create.to_s
-        table, tid = get_table_ancestor_id(node)
-        make_table_footnote_link(out, tid + fn, node.at(ns("./fmt-fn-label")))
-        # do not output footnote text if we have already seen it for this table
-        return if @seen_footnote.include?(tid + fn)
-
-        update_table_fn_body_ref(node, table, tid + fn)
-        @seen_footnote << (tid + fn)
       end
 
       def seen_footnote_parse(node, out, footnote)
