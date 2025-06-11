f2bc830f12b7c05a499cedef8be264d2e7899a08
Nick Nicholas <opoudjis@gmail.com>
2025-06-12 01:30:40 +1000

    redo SVG HTML for scaling: https://github.com/metanorma/iso-10303/issues/601

--- lib/isodoc/html_function/html.rb
+++ lib/isodoc/html_function/html.rb
@@ -102,6 +102,9 @@ module IsoDoc
 
       def image_body_parse(node, attrs, out)
         if svg = node.at("./m:svg", "m" => "http://www.w3.org/2000/svg")
+          if svg["height"].to_i > 0 && svg["width"].to_i > 0 && !svg["viewbox"]
+            svg["viewbox"] = "0 0 #{svg["width"} #{svg[height]}"
+          end
           #svg.delete("width")
           #svg["height"] = "1px"
           #svg["padding-bottom"] = "calc(100% * 3 / 4);"
