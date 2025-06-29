f3a3d59adf0eecde4c7336487eeee55855c21864
Nick Nicholas <opoudjis@gmail.com>
2025-06-30 00:27:43 +1000

    publisher logo size attributes: https://github.com/metanorma/metanorma-iso/issues/729

--- spec/isodoc/presentation_xml_spec.rb
+++ spec/isodoc/presentation_xml_spec.rb
@@ -2200,62 +2200,62 @@ RSpec.describe IsoDoc do
       </standard-document>
     INPUT
     presxml = <<~OUTPUT
-        <standard-document xmlns="https://www.metanorma.org/ns/standoc" type="presentation">
-           <bibdata/>
-           <preface>
-              <clause type="toc" id="_ad031967-2ffb-7abe-7724-fe42e3e69ab6" displayorder="1">
-                 <fmt-title depth="1" id="_94e961da-c46e-10c4-3633-df05fa9b249b">Table of contents</fmt-title>
-              </clause>
-              <floating-title original-id="_7a358acc-ba01-efef-79d3-ffddb271a5f2">FL 6</floating-title>
-              <p id="_6bc6edd4-ce02-3d09-6fb9-fe9a58b0add6" type="floating-title" displayorder="2">
-                 <semx element="floating-title" source="_6bc6edd4-ce02-3d09-6fb9-fe9a58b0add6">FL 6</semx>
-              </p>
-              <abstract id="_342ec181-36d5-e26a-f135-7a2ab9504cc7" displayorder="3"/>
-              <floating-title original-id="_5eb946b0-308e-6eba-08b6-6b4c87e08eed">FL 3</floating-title>
-              <p id="_b702b5ff-2973-3b1f-b5a1-ef974f0dcb53" type="floating-title" displayorder="4">
-                 <semx element="floating-title" source="_b702b5ff-2973-3b1f-b5a1-ef974f0dcb53">FL 3</semx>
-              </p>
-              <floating-title original-id="_9e7ab2e8-e33c-91c3-7744-66800e5b8a87">FL 4</floating-title>
-              <p id="_2eb9f685-30aa-f993-e7ce-77a904a5998c" type="floating-title" displayorder="5">
-                 <semx element="floating-title" source="_2eb9f685-30aa-f993-e7ce-77a904a5998c">FL 4</semx>
-              </p>
-              <foreword id="_92a0fd29-1e23-10a1-7ccf-79659e1d55c4" displayorder="6">
-                 <title id="_6ef51b59-4654-aa9d-dd44-776977643101">Foreword 1</title>
-                 <fmt-title depth="1" id="_78ad0ed3-d83f-8139-0bcd-200a2d61daf5">
-                    <semx element="title" source="_6ef51b59-4654-aa9d-dd44-776977643101">Foreword 1</semx>
-                 </fmt-title>
-              </foreword>
-              <floating-title original-id="_e448c5b5-a5e2-25ba-292b-bc6388f9c0bb">FL 5</floating-title>
-              <p id="_d01a6376-a126-1b02-481e-e66a095e91fd" type="floating-title" displayorder="7">
-                 <semx element="floating-title" source="_d01a6376-a126-1b02-481e-e66a095e91fd">FL 5</semx>
-              </p>
-              <foreword id="_35070999-7e5f-7e8f-cf70-1ca23584af50" displayorder="8">
-                 <title id="_bb6f6961-5f5e-540e-e2a9-bfd743bea25f">Foreword 2</title>
-                 <fmt-title depth="1" id="_d7ffd817-6fae-c526-d5cb-5b33e3f75cea">
-                    <semx element="title" source="_bb6f6961-5f5e-540e-e2a9-bfd743bea25f">Foreword 2</semx>
-                 </fmt-title>
-              </foreword>
-              <floating-title original-id="_6d170179-ba49-4af9-ed38-7987c1a7cbe0">FL 1</floating-title>
-              <p id="_f8a8f840-3950-a27d-a79d-7db536f9980a" type="floating-title" displayorder="9">
-                 <semx element="floating-title" source="_f8a8f840-3950-a27d-a79d-7db536f9980a">FL 1</semx>
-              </p>
-              <floating-title original-id="_a139118f-038f-e387-88cc-9dbf651bcbcb">FL 2</floating-title>
-              <p id="_9fdb84cf-58b7-1ac8-450f-29cfe0117412" type="floating-title" displayorder="10">
-                 <semx element="floating-title" source="_9fdb84cf-58b7-1ac8-450f-29cfe0117412">FL 2</semx>
-              </p>
-              <introduction id="_27e1812e-2d86-ea96-5220-c895b505e1aa" displayorder="11"/>
-              <floating-title original-id="_6cfccd90-6101-23b5-f708-728b534920f9">FL 0</floating-title>
-              <p id="_2b26a4c5-3893-fdd2-ab37-0be1cf6424d4" type="floating-title" displayorder="12">
-                 <semx element="floating-title" source="_2b26a4c5-3893-fdd2-ab37-0be1cf6424d4">FL 0</semx>
-              </p>
-              <acknowledgements id="_8afab461-7af9-b53a-d387-6ff08ef0e2d1" displayorder="13"/>
-              <floating-title original-id="_5622af3a-8c11-41a3-2998-1f268a74ac1c">FL 7</floating-title>
-              <p id="_c888c2f8-b742-0209-ffaa-d5783dec2c65" type="floating-title" displayorder="14">
-                 <semx element="floating-title" source="_c888c2f8-b742-0209-ffaa-d5783dec2c65">FL 7</semx>
-              </p>
-              <executivesummary id="_3d8ba6d3-5e1a-e024-7985-661f63bdab97" displayorder="15"/>
-           </preface>
-        </standard-document>
+      <standard-document xmlns="https://www.metanorma.org/ns/standoc" type="presentation">
+         <bibdata/>
+         <preface>
+            <clause type="toc" id="_ad031967-2ffb-7abe-7724-fe42e3e69ab6" displayorder="1">
+               <fmt-title depth="1" id="_94e961da-c46e-10c4-3633-df05fa9b249b">Table of contents</fmt-title>
+            </clause>
+            <floating-title original-id="_7a358acc-ba01-efef-79d3-ffddb271a5f2">FL 6</floating-title>
+            <p id="_6bc6edd4-ce02-3d09-6fb9-fe9a58b0add6" type="floating-title" displayorder="2">
+               <semx element="floating-title" source="_6bc6edd4-ce02-3d09-6fb9-fe9a58b0add6">FL 6</semx>
+            </p>
+            <abstract id="_342ec181-36d5-e26a-f135-7a2ab9504cc7" displayorder="3"/>
+            <floating-title original-id="_5eb946b0-308e-6eba-08b6-6b4c87e08eed">FL 3</floating-title>
+            <p id="_b702b5ff-2973-3b1f-b5a1-ef974f0dcb53" type="floating-title" displayorder="4">
+               <semx element="floating-title" source="_b702b5ff-2973-3b1f-b5a1-ef974f0dcb53">FL 3</semx>
+            </p>
+            <floating-title original-id="_9e7ab2e8-e33c-91c3-7744-66800e5b8a87">FL 4</floating-title>
+            <p id="_2eb9f685-30aa-f993-e7ce-77a904a5998c" type="floating-title" displayorder="5">
+               <semx element="floating-title" source="_2eb9f685-30aa-f993-e7ce-77a904a5998c">FL 4</semx>
+            </p>
+            <foreword id="_92a0fd29-1e23-10a1-7ccf-79659e1d55c4" displayorder="6">
+               <title id="_6ef51b59-4654-aa9d-dd44-776977643101">Foreword 1</title>
+               <fmt-title depth="1" id="_78ad0ed3-d83f-8139-0bcd-200a2d61daf5">
+                  <semx element="title" source="_6ef51b59-4654-aa9d-dd44-776977643101">Foreword 1</semx>
+               </fmt-title>
+            </foreword>
+            <floating-title original-id="_e448c5b5-a5e2-25ba-292b-bc6388f9c0bb">FL 5</floating-title>
+            <p id="_d01a6376-a126-1b02-481e-e66a095e91fd" type="floating-title" displayorder="7">
+               <semx element="floating-title" source="_d01a6376-a126-1b02-481e-e66a095e91fd">FL 5</semx>
+            </p>
+            <foreword id="_35070999-7e5f-7e8f-cf70-1ca23584af50" displayorder="8">
+               <title id="_bb6f6961-5f5e-540e-e2a9-bfd743bea25f">Foreword 2</title>
+               <fmt-title depth="1" id="_d7ffd817-6fae-c526-d5cb-5b33e3f75cea">
+                  <semx element="title" source="_bb6f6961-5f5e-540e-e2a9-bfd743bea25f">Foreword 2</semx>
+               </fmt-title>
+            </foreword>
+            <floating-title original-id="_6d170179-ba49-4af9-ed38-7987c1a7cbe0">FL 1</floating-title>
+            <p id="_f8a8f840-3950-a27d-a79d-7db536f9980a" type="floating-title" displayorder="9">
+               <semx element="floating-title" source="_f8a8f840-3950-a27d-a79d-7db536f9980a">FL 1</semx>
+            </p>
+            <floating-title original-id="_a139118f-038f-e387-88cc-9dbf651bcbcb">FL 2</floating-title>
+            <p id="_9fdb84cf-58b7-1ac8-450f-29cfe0117412" type="floating-title" displayorder="10">
+               <semx element="floating-title" source="_9fdb84cf-58b7-1ac8-450f-29cfe0117412">FL 2</semx>
+            </p>
+            <introduction id="_27e1812e-2d86-ea96-5220-c895b505e1aa" displayorder="11"/>
+            <floating-title original-id="_6cfccd90-6101-23b5-f708-728b534920f9">FL 0</floating-title>
+            <p id="_2b26a4c5-3893-fdd2-ab37-0be1cf6424d4" type="floating-title" displayorder="12">
+               <semx element="floating-title" source="_2b26a4c5-3893-fdd2-ab37-0be1cf6424d4">FL 0</semx>
+            </p>
+            <acknowledgements id="_8afab461-7af9-b53a-d387-6ff08ef0e2d1" displayorder="13"/>
+            <floating-title original-id="_5622af3a-8c11-41a3-2998-1f268a74ac1c">FL 7</floating-title>
+            <p id="_c888c2f8-b742-0209-ffaa-d5783dec2c65" type="floating-title" displayorder="14">
+               <semx element="floating-title" source="_c888c2f8-b742-0209-ffaa-d5783dec2c65">FL 7</semx>
+            </p>
+            <executivesummary id="_3d8ba6d3-5e1a-e024-7985-661f63bdab97" displayorder="15"/>
+         </preface>
+      </standard-document>
     OUTPUT
     expect(Xml::C14n.format(IsoDoc::PresentationXMLConvert
       .new(presxml_options)
@@ -2355,5 +2355,53 @@ RSpec.describe IsoDoc do
     log.write("test.err.html")
     html = File.read("test.err.html")
     expect(html).to include "Anchor B pointed to by fmt-title is not defined in the document"
+  end
+
+  it "completes incomplete logo presentation metadata" do
+    input = <<~INPUT
+      <iso-standard xmlns="http://riboseinc.com/isoxml">
+        <bibdata>
+        <title language="en">test</title>
+        </bibdata>
+        <metanorma-extension>
+          <presentation-metadata><logo-fred-height>4</logo-fred-height></presentation-metadata>
+          <presentation-metadata><logo-author-pdf-height>4</logo-author-pdf-height></presentation-metadata>
+          <presentation-metadata><logo-author-height>4</logo-author-height></presentation-metadata>
+        </metanorma-extension>
+      </iso-standard
+    INPUT
+    presxml = <<~OUTPUT
+      <iso-standard xmlns="http://riboseinc.com/isoxml" type="presentation">
+          <bibdata>
+             <title language="en">test</title>
+          </bibdata>
+          <metanorma-extension>
+             <presentation-metadata>
+                <logo-fred-height>4</logo-fred-height>
+             </presentation-metadata>
+             <presentation-metadata>
+                <logo-author-pdf-height>4</logo-author-pdf-height>
+             </presentation-metadata>
+             <presentation-metadata>
+                <logo-author-height>4</logo-author-height>
+             </presentation-metadata>
+             <presentation-metadata>
+                <logo-author-html-height>4</logo-author-html-height>
+             </presentation-metadata>
+             <presentation-metadata>
+                <logo-author-pdf-height>4</logo-author-pdf-height>
+             </presentation-metadata>
+             <presentation-metadata>
+                <logo-author-doc-height>4</logo-author-doc-height>
+             </presentation-metadata>
+          </metanorma-extension>
+       </iso-standard>
+    OUTPUT
+    expect(Xml::C14n.format(IsoDoc::PresentationXMLConvert
+       .new(presxml_options.merge({ output_formats: { doc: "DOC", pdf: "PDF",
+                                                      html: "HTML" } }))
+       .convert("test", input, true)
+       .sub(%r{<localized-strings>.*</localized-strings>}m, "")))
+      .to be_equivalent_to Xml::C14n.format(presxml)
   end
 end
