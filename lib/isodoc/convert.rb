3fb5a4f71dd6176bfae6128b7ad21bcc6c1615b9
Nick Nicholas <opoudjis@gmail.com>
2025-08-04 11:48:25 +1000

    version bump

--- lib/isodoc/convert.rb
+++ lib/isodoc/convert.rb
@@ -11,7 +11,7 @@ module IsoDoc
   class Convert < ::IsoDoc::Common
     attr_accessor :options, :i18n, :meta, :xrefs, :reqt_models,
                   :requirements_processor, :doctype, :bibrender,
-                  :tempfile_cache, :wordcoverpage, :wordintropage
+                  :tempfile_cache, :wordcoverpage, :wordintropage, :localdir
 
     # htmlstylesheet: Generic stylesheet for HTML
     # htmlstylesheet_override: Override stylesheet for HTML
