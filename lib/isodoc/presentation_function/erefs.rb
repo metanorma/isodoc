1b9269f5413a874f9432ebbc29298b65214070c8
Nick Nicholas <opoudjis@gmail.com>
2025-10-13 10:54:57 +1100

    refactor

--- lib/isodoc/presentation_function/erefs.rb
+++ lib/isodoc/presentation_function/erefs.rb
@@ -1,4 +1,5 @@
 require "metanorma-utils"
+require_relative "erefs_locality"
 
 module IsoDoc
   class PresentationXMLConvert < ::IsoDoc::Convert
@@ -37,36 +38,7 @@ module IsoDoc
       elem.next = "<semx element='erefstack' source='#{elem['id']}'>#{l10n ret[1]}</semx>"
     end
 
-    def eref_localities(refs, target, node)
-      if can_conflate_eref_rendering?(refs)
-        l10n(", <esc>#{eref_localities_conflated(refs, target, node)}</esc>"
-          .gsub(/\s+/, " "), @lang, @script, { prev: target })
-      else
-        ret = resolve_eref_connectives(eref_locality_stacks(refs, target, node))
-        l10n(ret.join.gsub(/\s+/, " "), @lang, @script, { prev: target })
-      end
-    end
-
-    def eref_localities_conflated(refs, target, node)
-      droploc = node["droploc"]
-      node["droploc"] = true
-      ret = resolve_eref_connectives(eref_locality_stacks(refs, target, node))
-      node.delete("droploc") unless droploc
-      eref_localities1({ target:, number: "pl",
-                         type: refs.first.at(ns("./locality/@type")).text,
-                         from: l10n(ret[1..].join), node:, lang: @lang })
-    end
-
-    def can_conflate_eref_rendering?(refs)
-      (refs.size > 1 &&
-        refs.all? { |r| r.name == "localityStack" } &&
-        refs.all? { |r| r.xpath(ns("./locality")).size == 1 }) or return false
-      first = refs.first.at(ns("./locality/@type")).text
-      refs.all? { |r| r.at(ns("./locality/@type")).text == first }
-    end
-
     def resolve_eref_connectives(locs)
-      #require 'debug'; binding.b
       locs = escape_l10n(locs)
       locs = resolve_comma_connectives(locs)
       locs = resolve_to_connectives(locs)
@@ -77,9 +49,11 @@ module IsoDoc
       [", ", combine_conn(locs)]
     end
 
+    XREF_CONNECTIVES = %w(from to or and).freeze
+
     def escape_l10n(locs)
       locs.map do |x|
-        if ["from", "to", "or", "and", ", ", " ", ""].include?(x)
+        if XREF_CONNECTIVES.include?(x) || [", ", " ", ""].include?(x)
           x
         else
           "<esc>#{x}</esc>"
@@ -120,89 +94,6 @@ module IsoDoc
         end
       end
       locs1
-    end
-
-    def eref_locality_stacks(refs, target, node)
-      #require "debug"; binding.b
-      ret = refs.each_with_index.with_object([]) do |(r, i), m|
-        added = eref_locality_stack(r, i, target, node)
-        added.empty? and next
-        added.each { |a| m << a }
-        i == refs.size - 1 and next
-        m << eref_locality_delimiter(r)
-      end
-      ret.empty? ? ret : [", "] + ret
-    end
-
-    def eref_locality_delimiter(ref)
-      if ref&.next_element&.name == "localityStack"
-        ref.next_element["connective"]
-      else locality_delimiter(ref)
-      end
-    end
-
-    def eref_locality_stack(ref, idx, target, node)
-      ret = []
-      if ref.name == "localityStack"
-        ret = eref_locality_stack1(ref, target, node, ret)
-      else
-        l = eref_localities0(ref, idx, target, node) and ret << l
-      end
-      ret[-1] == ", " and ret.pop
-      ret
-    end
-
-    def eref_locality_stack1(ref, target, node, ret)
-      ref.elements.each_with_index do |rr, j|
-        l = eref_localities0(rr, j, target, node) or next
-        ret << l
-        ret << locality_delimiter(rr) unless j == ref.elements.size - 1
-      end
-      ret
-    end
-
-    def locality_delimiter(_loc)
-      ", "
-    end
-
-    def eref_localities0(ref, _idx, target, node)
-      if ref["type"] == "whole" then @i18n.wholeoftext
-      else
-        eref_localities1({ target:, type: ref["type"], number: "sg",
-                           from: ref.at(ns("./referenceFrom"))&.text,
-                           upto: ref.at(ns("./referenceTo"))&.text, node:,
-                           lang: @lang })
-      end
-    end
-
-    def eref_localities1_zh(opt)
-      ret = "第<esc>#{opt[:from]}</esc>" if opt[:from]
-      ret += "&#x2013;<esc>#{opt[:upto]}</esc>" if opt[:upto]
-      loc = eref_locality_populate(opt[:type], opt[:node], "sg")
-      ret += " #{loc}" unless opt[:node]["droploc"] == "true"
-      ret
-    end
-
-    def eref_localities1(opt)
-      opt[:type] == "anchor" and return nil
-      %(zh ja ko).include?(opt[:lang]) and
-        return l10n(eref_localities1_zh(opt))
-      ret = eref_locality_populate(opt[:type], opt[:node], opt[:number])
-      ret += " #{opt[:from]}" if opt[:from]
-      ret += "&#x2013;#{opt[:upto]}" if opt[:upto]
-      l10n(ret)
-    end
-
-    def eref_locality_populate(type, node, number)
-      node["droploc"] == "true" and return ""
-      loc = type.sub(/^locality:/, "")
-      ret = @i18n.locality[loc] || loc
-      number == "pl" and ret = @i18n.inflect(ret, number: "pl")
-      ret = case node["case"]
-            when "lowercase" then ret.downcase
-            else Metanorma::Utils.strict_capitalize_first(ret)
-            end
-      " #{ret}"
     end
 
     def eref2link(docxml)
---
+++ lib/isodoc/presentation_function/erefs_locality.rb
@@ -0,0 +1,113 @@
+module IsoDoc
+  class PresentationXMLConvert < ::IsoDoc::Convert
+    def eref_localities(refs, target, node)
+      if can_conflate_eref_rendering?(refs)
+        l10n(", <esc>#{eref_localities_conflated(refs, target, node)}</esc>"
+          .gsub(/\s+/, " "), @lang, @script, { prev: target })
+      else
+        ret = resolve_eref_connectives(eref_locality_stacks(refs, target, node))
+        l10n(ret.join.gsub(/\s+/, " "), @lang, @script, { prev: target })
+      end
+    end
+
+    def eref_localities_conflated(refs, target, node)
+      droploc = node["droploc"]
+      node["droploc"] = true
+      ret = resolve_eref_connectives(eref_locality_stacks(refs, target, node))
+      node.delete("droploc") unless droploc
+      eref_localities1({ target:, number: "pl",
+                         type: refs.first.at(ns("./locality/@type")).text,
+                         from: l10n(ret[1..].join), node:, lang: @lang })
+    end
+
+    def can_conflate_eref_rendering?(refs)
+      (refs.size > 1 &&
+        refs.all? { |r| r.name == "localityStack" } &&
+        refs.all? { |r| r.xpath(ns("./locality")).size == 1 }) or return false
+      first = refs.first.at(ns("./locality/@type")).text
+      refs.all? { |r| r.at(ns("./locality/@type")).text == first }
+    end
+
+    def eref_locality_stacks(refs, target, node)
+      ret = refs.each_with_index.with_object([]) do |(r, i), m|
+        added = eref_locality_stack(r, i, target, node)
+        added.empty? and next
+        added.each { |a| m << a }
+        i == refs.size - 1 and next
+        m << eref_locality_delimiter(r)
+      end
+      ret.empty? ? ret : [", "] + ret
+    end
+
+    def eref_locality_delimiter(ref)
+      if ref&.next_element&.name == "localityStack"
+        ref.next_element["connective"]
+      else locality_delimiter(ref)
+      end
+    end
+
+    def eref_locality_stack(ref, idx, target, node)
+      ret = []
+      if ref.name == "localityStack"
+        ret = eref_locality_stack1(ref, target, node, ret)
+      else
+        l = eref_localities0(ref, idx, target, node) and ret << l
+      end
+      ret[-1] == ", " and ret.pop
+      ret
+    end
+
+    def eref_locality_stack1(ref, target, node, ret)
+      ref.elements.each_with_index do |rr, j|
+        l = eref_localities0(rr, j, target, node) or next
+        ret << l
+        ret << locality_delimiter(rr) unless j == ref.elements.size - 1
+      end
+      ret
+    end
+
+    def locality_delimiter(_loc)
+      ", "
+    end
+
+    def eref_localities0(ref, _idx, target, node)
+      if ref["type"] == "whole" then @i18n.wholeoftext
+      else
+        eref_localities1({ target:, type: ref["type"], number: "sg",
+                           from: ref.at(ns("./referenceFrom"))&.text,
+                           upto: ref.at(ns("./referenceTo"))&.text, node:,
+                           lang: @lang })
+      end
+    end
+
+    def eref_localities1_zh(opt)
+      ret = "第<esc>#{opt[:from]}</esc>" if opt[:from]
+      ret += "&#x2013;<esc>#{opt[:upto]}</esc>" if opt[:upto]
+      loc = eref_locality_populate(opt[:type], opt[:node], "sg")
+      ret += " #{loc}" unless opt[:node]["droploc"] == "true"
+      ret
+    end
+
+    def eref_localities1(opt)
+      opt[:type] == "anchor" and return nil
+      %(zh ja ko).include?(opt[:lang]) and
+        return l10n(eref_localities1_zh(opt))
+      ret = eref_locality_populate(opt[:type], opt[:node], opt[:number])
+      ret += " #{opt[:from]}" if opt[:from]
+      ret += "&#x2013;#{opt[:upto]}" if opt[:upto]
+      l10n(ret)
+    end
+
+    def eref_locality_populate(type, node, number)
+      node["droploc"] == "true" and return ""
+      loc = type.sub(/^locality:/, "")
+      ret = @i18n.locality[loc] || loc
+      number == "pl" and ret = @i18n.inflect(ret, number: "pl")
+      ret = case node["case"]
+            when "lowercase" then ret.downcase
+            else Metanorma::Utils.strict_capitalize_first(ret)
+            end
+      " #{ret}"
+    end
+  end
+end
