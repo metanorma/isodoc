cfc0dc1f53b28e3261afc3692de72e07dc2a2afb
Nick Nicholas <opoudjis@gmail.com>
2025-12-01 20:39:05 +1100

    escape draft number in Metadata: https://github.com/metanorma/metanorma-plateau/issues/146

--- lib/isodoc/metadata.rb
+++ lib/isodoc/metadata.rb
@@ -113,7 +113,7 @@ module IsoDoc
     def draftinfo(draft, revdate)
       return "" unless draft
 
-      draftinfo = " (#{@labels['draft_label']} #{draft}"
+      draftinfo = " (<esc>#{@labels['draft_label']}</esc> #{draft}"
       draftinfo += ", #{revdate}" if revdate
       draftinfo += ")"
       l10n(draftinfo, @lang, @script)
